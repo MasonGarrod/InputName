3f57306f9af38a62ea834e13bc6b3e3d8868162e
MasonGarrod <113171947+MasonGarrod@users.noreply.github.com>
2022-09-26 10:59:54 +0100

    Add files via upload

---
+++ InputName!/InputName!/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputName_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name;
+
+            Console.WriteLine("Enter your name: ");
+            name = Console.ReadLine();
+
+            while (name == "")
+            {
+                Console.WriteLine("Invalid input, enter your name again: ");
+                name = Console.ReadLine();
+
+            }
+
+            if (name == name)
+            {
+                Console.WriteLine("Your name is " + name);
+            }
+
+            Console.WriteLine("Press any key to close");
+            Console.ReadKey();
+
+        }
+    }
+}
